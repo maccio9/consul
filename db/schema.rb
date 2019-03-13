5c8676ab9794fdc0d68bce561c0202c6fb7d25f4
Gaston Arbeletche <garbeletche@vairix.com>
2019-03-13 17:22:58 -0300

    add schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180611164600) do
+ActiveRecord::Schema.define(version: 20190313201852) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -1112,6 +1112,7 @@ ActiveRecord::Schema.define(version: 20180611164600) do
     t.string   "in_place"
     t.string   "surname"
     t.boolean  "terms_of_service"
+    t.text     "log_text"
   end
 
   add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true, using: :btree
